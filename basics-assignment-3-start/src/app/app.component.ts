b03641d30b0332954466e666328895159f31621a
Gianpierre Fernandez <Gianpi@Gianpierre-MacBook-Pro.local>
2019-10-14 01:34:15 +0200

    add logic and cosmetic styling

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -3,7 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
+  
 })
 export class AppComponent {
+  displaying : boolean = false;
+  contador : number = 0;
+  clicks = [];
+
+  onShowPhrase() {
+    // this.displaying ? this.displaying = false : this.displaying = true;
+    this.displaying = !this.displaying;
+    this.contador ++;
+    this.clicks.push(this.contador);
+    
+  } 
+  
+  
+
 }
+
